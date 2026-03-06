a7303925062731ebb6abd60ec6b6783ed6422614
Dylan Kainth <kainth.dylan@gmail.com>
2026-03-06 14:43:05 +0000

    feat: refactor counter module and add testbench

--- week2/counter/counter_example.v
+++ week2/counter/counter_example.v
@@ -1,11 +1,22 @@
-reg [25:0] count; // Our 26-bit counter
-reg [3:0]  value; // The 4-bit number for our 7-seg
+module counter_example(
+    input  wire clk,
+    output reg [3:0] value
+);
 
-always @(posedge clk) begin
-    if (count == 50_000_000) begin
-        count < = 0;          // Reset counter
-        value < = value + 1;  // Increment the display digit
-    end else begin
-        count < = count + 1;  // Keep waiting
+    reg [25:0] count; // Our 26-bit counter
+
+    initial begin
+        count = 0;
+        value = 0;
     end
-end
+
+    always @(posedge clk) begin
+        if (count == 50_000_000) begin
+            count <= 0;          // Reset counter
+            value <= value + 1;  // Increment the display digit
+        end else begin
+            count <= count + 1;  // Keep waiting
+        end
+    end
+
+endmodule
